8893fa60fb5cb732c3f65dbfcc4db7a4b9d47f76
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-11 12:18:20 -0800

    Reformat plugin.cpp (with clang-format)

--- plugin.cpp
+++ plugin.cpp
@@ -1,13 +1,12 @@
 SKSEPluginLoad(const SKSE::LoadInterface *skse) {
-  SKSE::Init(skse);
+    SKSE::Init(skse);
 
-  // Once all plugins and mods are loaded, then the ~ console is ready and can
-  // be printed to
-  SKSE::GetMessagingInterface()->RegisterListener(
-      [](SKSE::MessagingInterface::Message *message) {
+    // Once all plugins and mods are loaded, then the ~ console is ready and can
+    // be printed to
+    SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message *message) {
         if (message->type == SKSE::MessagingInterface::kDataLoaded)
-          RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
-      });
+            RE::ConsoleLog::GetSingleton()->Print("Hello, world!");
+    });
 
-  return true;
+    return true;
 }
